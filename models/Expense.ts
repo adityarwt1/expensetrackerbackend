c74d92e46e31ba00e87afdf6c415058611636083
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-10 16:14:45 +0530

    expense tracker scheme id done

---
+++ models/Expense.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface Expense extends Document {
+  userid: mongoose.Types.ObjectId;
+  title: string;
+  amount: number;
+  method?: "us-dollar" | "inr";
+}
+
+const ExpenseSchema: Schema<Expense> = new Schema(
+  {
+    userid: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    method: {
+      type: String,
+      enum: ["us-dollar", "inr"], // only two allowed values
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+ExpenseSchema.index({ userid: 1 });
+
+const Expense =
+  mongoose.models.Expense || mongoose.model<Expense>("Expense", ExpenseSchema);
+
+export default Expense;
