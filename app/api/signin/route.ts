b77faf1adbe63b855bf5008f24f7da879706c5fb
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-16 22:09:44 +0530

    sign in page logic is done:

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -2,7 +2,11 @@ import { DBconnect, DBdisconnect } from "@/lib/mongobd";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
 export async function POST(request: NextRequest) {
   try {
     const { email, password } = await request.json();
@@ -13,11 +17,7 @@ export async function POST(request: NextRequest) {
         { error: "Bad request" },
         {
           status: 400,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Headers": "Content-Type",
-          },
+          headers: corsHeaders
         }
       );
     }
@@ -33,11 +33,7 @@ export async function POST(request: NextRequest) {
         { error: "User not found" },
         {
           status: 404,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Headers": "Content-Type",
-          },
+          headers: corsHeaders
         }
       );
     }
@@ -49,11 +45,7 @@ export async function POST(request: NextRequest) {
         { error: "Wrong Password" },
         {
           status: 400,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Headers": "Content-Type",
-          },
+          headers: corsHeaders
         }
       );
     }
@@ -63,11 +55,7 @@ export async function POST(request: NextRequest) {
       { message: "Hellow from the cors api", user },
       {
         status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
+        headers: corsHeaders
       }
     );
   } catch (error) {
@@ -77,12 +65,16 @@ export async function POST(request: NextRequest) {
       { error: "Internal server issue" },
       {
         status: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
+        headers: corsHeaders
       }
     );
   }
+}
+
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    { message: "CORS preflight successful" },
+    { status: 200, headers: corsHeaders }
+  );
 }
